01ba5878bdb625d7edd96edcd314a9b68446f87e
ShubHH03 <mak.shubham2003@gmail.com>
2025-07-31 19:49:28 +0530

    get jokes with id

--- backend/server.js
+++ backend/server.js
@@ -8,40 +8,38 @@ const app = express();
 
 // get a list of 5 jokes
 
+const jokes = [
+  {
+    id: 1,
+    title: "joke 1",
+    content:
+      "Why did the scarecrow win an award? Because he was outstanding in his field!",
+  },
+  {
+    id: 2,
+    title: "joke 2",
+    content:
+      "Why don't scientists trust atoms? Because they make up everything!",
+  },
+  {
+    id: 3,
+    title: "joke 3",
+    content: "What do you call fake spaghetti? An impasta!",
+  },
+];
+
 app.get("/api/jokes", (req, res) => {
-  const jokes = [
-    {
-      id: 1,
-      title: "joke 1",
-      content:
-        "Why did the scarecrow win an award? Because he was outstanding in his field!",
-    },
-    {
-      id: 2,
-      title: "joke 2",
-      content:
-        "Why don't scientists trust atoms? Because they make up everything!",
-    },
-    {
-      id: 3,
-      title: "joke 3",
-      content:
-        "What do you call fake spaghetti? An impasta!",
-    },
-    {
-      id: 4,
-      title: "joke 4",
-      content:
-        "Why did the bicycle fall over? Because it was two-tired!",
-    },
-    {
-      id: 5,
-      title: "joke 5",
-      content:
-        "What do you call cheese that isn't yours? Nacho cheese!",
-    },
-  ];
   res.send(jokes);
+});
+
+app.get("/api/jokes/:id", (req, res) => {
+  const jokeId = parseInt(req.params.id, 10);
+  const joke = jokes.find((j) => j.id === jokeId);
+  if (joke) {
+    res.send(joke);
+  } else {
+    res.status(404).send({ message: "Joke not found" });
+  }
 });
 
 const port = process.env.PORT || 3000;
